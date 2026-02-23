8cecdb22bcee6cad75e6c5dcdefbb035f0657545
codewithstella <coderluffy35@gmail.com>
2026-02-23 08:44:07 +0530

    06-Background jobs

---
+++ src/inngest/functions.ts
@@ -0,0 +1,18 @@
+import { inngest } from "./client";
+import prisma from "@/lib/db";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "10s");
+    
+    await step.run("create-workflow", async () => {
+        await prisma.workflow.create({
+            data: {
+                name: event.data.email,
+            },
+        });
+    });
+  }
+);
