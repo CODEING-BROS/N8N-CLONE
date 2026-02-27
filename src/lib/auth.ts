caf63ba81d403284f9426bd2631bb99b79118f29
codewithstella <coderluffy35@gmail.com>
2026-02-27 11:39:09 +0530

    Payments: Setup Polar, Better Auth, checkout, billing portal. Updated dev phase in README and sequence diagrams.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
+import { polar, checkout, portal } from "@polar-sh/better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import polarClient from "./polar";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -9,5 +11,24 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
         autoSignIn: true,
-    }
+    },
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId: "12b4acd1-e17c-4c65-8ec0-e74da4d1c3ff",
+                            slug: "Nodebase-Pro"
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true
+                }),
+                portal()
+            ],
+        }),
+    ],
 });
